86a59ed747a37e6038919669b80d7f939c589773
Jessé Benício <cdcssloj@gmail.com>
2025-04-07 22:22:03 -0300

    add_ideias_2

--- ideias/sites/1 - Desenvolvimento de sites/tela login.js
+++
@@ -1,8 +0,0 @@
-
-const toggleBox = document.querySelector('.toggle-box');
-const loginBox = document.querySelector('.login-box');
-const signupBox = document.querySelector('.signup-box');
-
-toggleBox.addEventListener('click', () => {
-  toggleBox.classList.toggle('active');
-});
